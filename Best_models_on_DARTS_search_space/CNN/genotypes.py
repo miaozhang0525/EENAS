9a3cab2b8ff11b1ff2d12fcf7790e632226bd369
MiaoZhang0525 <MiaoZhang0525@163.com>
2020-12-13 15:43:39 +1100

    update

--- Best_models_on_DARTS_search_space/CNN/genotypes.py
+++ Best_models_on_DARTS_search_space/CNN/genotypes.py
@@ -95,14 +95,13 @@ EENAS = Genotype(
 
 EENAS_R = Genotype(
 	normal=[
-	('sep_conv_3x3', 0), ('sep_conv_3x3', 1), 
-	('sep_conv_3x3', 1), ('sep_conv_3x3', 2), 
-	('sep_conv_3x3', 0), ('sep_conv_3x3', 3), 
-	('skip_connect', 0), ('dil_conv_5x5', 4)], 
+	('skip_connect', 0), ('dil_conv_5x5', 1), 
+	('sep_conv_3x3', 0), ('sep_conv_3x3', 2), 
+	('sep_conv_3x3', 1), ('sep_conv_3x3', 3), 
+	('sep_conv_3x3', 0), ('dil_conv_5x5', 4)], 
 	normal_concat=range(2, 6), 
 	reduce=[
-	('max_pool_3x3', 0), ('dil_conv_5x5', 1), 
+	('avg_pool_3x3', 0), ('sep_conv_3x3', 1), 
 	('avg_pool_3x3', 0), ('dil_conv_5x5', 2), 
-	('max_pool_3x3', 1), ('dil_conv_5x5', 3), 
-	('avg_pool_3x3', 0), ('dil_conv_5x5', 4)], 
-	reduce_concat=[2, 3, 4, 5])	
+	('max_pool_3x3', 0), ('dil_conv_5x5', 3), 
+	('max_pool_3x3', 1), ('dil_conv_3x3', 4)], reduce_concat=range(2, 6))
